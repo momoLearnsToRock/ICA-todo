7cad4b098e56553209d28411a207b3545ccbc142
momoLearnsToRock <haratiasl@gmail.com>
2018-05-07 13:42:13 +0200

    categories post, initial select

---
+++ TodoService/js/routers/routerBase.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class RouterBase {
+    constructor() {
+    }
+}
+exports.RouterBase = RouterBase;
+//# sourceMappingURL=routerBase.js.map
